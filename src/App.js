13eedf1f64e9c6c296fcf302002058bf73e545de
ReemKhalil18499 <khalil.reem18499@gmail.com>
2021-09-05 22:48:53 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import axios from "axios";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Image from 'react-bootstrap/Image';
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,33 +13,53 @@ class App extends React.Component {
       City: "",
       lat: "",
       long: "",
+      inspect: false,
     };
   }
   New = async (event) => {
     event.preventDefault();
-    const City = event.target.location.value;
+    const City = event.target.City.value;
     const Link = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${City}&format=json`;
-    const Reply = await axios.get(Link);
-    this.setState({
-      location: Reply.data[0].display_name,
-      long: Reply.data[0].long,
-      lat: Reply.data[0].lat,
-    });
+    try {
+      const Reply = await axios.get(Link);
+      this.setState({
+        City: Reply.data[0].display_name,
+        long: Reply.data[0].lon,
+        lat: Reply.data[0].lat,
+      });
+    } catch {
+      this.setState({
+        inspect:true,
+      })
+    }
   };
 
   render() {
     return (
       <>
-        <Form>
+        <Form onSubmit={this.New}>
           <Form.Group className="mb-3" controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control name="City" type="text" placeholder="Enter a City" />
+            <Form.Label className="city">City</Form.Label>
+            <Form.Control
+              className="input"
+              name="City"
+              type="text"
+              placeholder="Enter a City"
+            />
           </Form.Group>
 
           <Button variant="primary" type="submit">
             Explore!
           </Button>
         </Form>
+        {this.state.inspect?<p>Status Code: 400, 404, 500</p>:<p></p>}
+        <p>
+          City : {this.state.City}, lat:{this.state.lat}, long:
+          {this.state.long}
+        </p>
+        <Image
+          src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.lat},${this.state.long}&zoom=1-18`}
+        />
       </>
     );
   }
